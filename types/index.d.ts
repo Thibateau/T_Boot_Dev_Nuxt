2d60fe46e7fd72beeddf1d3f00e1d174b5c20b68
Thibaut Plichon <95209927+Thibateau@users.noreply.github.com>
2022-05-03 11:17:33 +0200

    Revert "Fix/responsive on project pag (#7)"
    This reverts commit dd5d4d1b595fbb0f8a2aa51fbf3d11aad3cb5dbb.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,82 +1,82 @@
 
 // NAVBAR
 export interface ILeftItems {
-label: string;
-name:string;
+	label: string;
+	name:string;
 }
 export interface IRightItems {
-label: string;
-name:string;
+	label: string;
+	name:string;
 }
 export interface INavBar{
-left : ILeftItems[]
-right : IRightItems[]
-logo: string
+	left : ILeftItems[]
+	right : IRightItems[]
+	logo: string
 }
 
 // INDEX PAGE
 export interface ILeftHero {
-picture : string,
-title: string,
-subtitle: string,
-paragraph: string
+	picture : string,
+	title: string,
+	subtitle: string,
+	paragraph: string
 }
 export interface ICta{
-label: string,
-url: string
+	label: string,
+	url: string
 }
 export interface IRightHero {
-picture : string,
-title: string,
-subtitle: string,
-label: string,
+	picture : string,
+	title: string,
+	subtitle: string,
+	label: string,
 }
 
 // ABOUT PAGE
 export interface ISocial{
-url : string,
-label : string
+	url : string,
+	label : string
 }
 export interface IPictureHero{
-title : string,
-subtitle : string,
-paragraph: string,
-socials : ISocial[],
-picture : string
+	title : string,
+	subtitle : string,
+	paragraph: string,
+	socials : ISocial[],
+	picture : string
 }
 export interface IToolList {
-picture : string,
-label: string
+	picture : string,
+	label: string
 }
 export interface IToolsBox{
-title : string,
-paragraph : string,
-toolsitems: IToolList[]
+	title : string,
+	paragraph : string,
+	toolsitems: IToolList[]
 }
 
 // PROJECTS PAGE
 export interface ICartoonHero {
-picture : string,
-title: string,
-subtitle: string,
-label: string,
+	picture : string,
+	title: string,
+	subtitle: string,
+	labe: string,
 }
 export interface WorkItem{
-picture:string,
-title : string,
-text: string
+	picture:string,
+	title : string,
+	text: string
 }
 export interface IProjectPresentation{
-title: string,
-text: string,
-works: WorkItem[]
+	title: string,
+	text: string,
+	works: WorkItem[]
 }
 // FOOTER
 export interface IFooter {
-title:string,
-subtitle: string,
-items: ILeftItems[] & IRightItems[],
-logo:string,
-rights: string,
-social: ISocial[]
+	title:string,
+	subtitle: string,
+	items: ILeftItems[] & IRightItems[],
+	logo:string,
+	rights: string,
+	social: ISocial[]
 }
